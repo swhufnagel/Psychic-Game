dbae8bcc4e26e10d810f4b06a7e5555bb5a30b87
Sean Huff <swhufnagel@gmail.com>
2019-03-23 15:15:44 -0600

    complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
 
 var wins = 0;
 var losses = 0;
-var guesses = 5;
-var chosen = null;
+var guesses = 9;
+var chosen = [];
 var computer = "";
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
-var playerChoice = "";
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var playerChoice = "None";
 
 computer = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log(computer);
 
 document.getElementById('wins').innerHTML = "Wins: " + wins;
 document.getElementById('loses').innerHTML = "Losses: " + losses;
-document.getElementById('guessleft').innerHTML = "Guess left: " + guesses;
+document.getElementById('guessleft').innerHTML = "Guesses left: " + guesses;
 document.getElementById('guessed').innerHTML = "Your guesses so far: " + chosen;
 
 document.onkeyup = function (event) {
@@ -20,28 +20,39 @@ document.onkeyup = function (event) {
     console.log("player: " + playerChoice);
     console.log("computer: " + computer);
 
-    if(playerChoice === computer){
-        wins+=1;
-        alert("You won! :)")
+    if (playerChoice === computer) {
+        wins += 1;
         document.getElementById('wins').innerHTML = "Wins: " + wins;
-    }
-    if(playerChoice !== computer){
-        guesses-=1;
-        document.getElementById('guessleft').innerHTML = "Guess left: " + guesses;
-        chosen = chosen + " " + playerChoice;
+        computer = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(computer);
+        alert("You won! :)")
+        guesses = 9;
+        document.getElementById('guessleft').innerHTML = "Guesses left: " + guesses;
+        chosen = [];
         document.getElementById('guessed').innerHTML = "Your guesses so far: " + chosen;
     }
-    if(guesses === 0){
-        losses+=1;
+    else if (alphabet.indexOf(playerChoice)> -1) {
+
+        if (playerChoice !== computer) {
+            guesses -= 1;
+            document.getElementById('guessleft').innerHTML = "Guesses left: " + guesses;
+            chosen.push(playerChoice)
+            document.getElementById('guessed').innerHTML = "Your guesses so far: " + chosen;
+        }
+    }
+    if(alphabet.indexOf(playerChoice) === -1){
+        alert("Please choose a letter in the alphabet");
+    }
+    if (guesses === 0) {
+        losses += 1;
         alert("You lost! :(")
-        guesses +=4
+        guesses += 9
         document.getElementById('loses').innerHTML = "Loses: " + losses;
-        chosen = "None";
+        document.getElementById('guessleft').innerHTML = "Guesses left: " + guesses;
+        chosen = [];
+        document.getElementById('guessed').innerHTML = "Your guesses so far: " + chosen;
+        computer = alphabet[Math.floor(Math.random() * alphabet.length)];
+        console.log(computer);
     }
 
 }
-
-document.getElementById('wins').innerHTML = "Wins: " + wins;
-document.getElementById('loses').innerHTML = "Loses: " + losses;
-document.getElementById('guessleft').innerHTML = "Guess left: " + guesses;
-document.getElementById('guessed').innerHTML = "Your guesses so far: " + chosen;
